e29020fdf282dce041756a7f9ede6e01b53128f3
Vlad <7nomer7@gmail.com>
2025-09-15 16:13:30 +0300

    Solution

--- taxi/forms.py
+++ taxi/forms.py
@@ -31,6 +31,11 @@ class DriverLicenseUpdateForm(forms.ModelForm):
     class Meta:
         model = Driver
         fields = ["license_number"]
-        widgets = {
-            "license_number": forms.CheckboxSelectMultiple(),
-        }
+
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+        if len(license_number) != 8:
+            raise forms.ValidationError("License must be 8 characters long.")
+        if not re.match(r"^[A-Z]{3}\d{5}$", license_number):
+            raise forms.ValidationError("License must start with 3 uppercase letters followed by 5 digits.")
+        return license_number
