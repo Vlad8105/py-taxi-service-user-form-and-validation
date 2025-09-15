a7e747c4a1a4a76409720df7da02ce7ac9136b74
Vlad <7nomer7@gmail.com>
2025-09-15 15:48:19 +0300

    Solution

--- tests/test_taxi_service_user_form_and_validation.py
+++ tests/test_taxi_service_user_form_and_validation.py
@@ -40,7 +40,7 @@ class DriverViewsTest(TestCase):
     def test_update_driver_license_number_with_valid_data(self):
         test_license_number = "ADM22345"
         response = self.client.post(
-            reverse("taxi:driver-update-license", kwargs={"pk": self.user.id}),
+            reverse("taxi:driver-update", kwargs={"pk": self.user.id}),
             data={"license_number": test_license_number},
         )
         self.assertEqual(response.status_code, 302)
@@ -48,7 +48,7 @@ class DriverViewsTest(TestCase):
     def test_update_driver_license_number_with_not_valid_data(self):
         test_license_number = "a5"
         response = self.client.post(
-            reverse("taxi:driver-update-license", kwargs={"pk": self.user.id}),
+            reverse("taxi:driver-update", kwargs={"pk": self.user.id}),
             data={"license_number": test_license_number},
         )
         self.assertEqual(response.status_code, 200)
